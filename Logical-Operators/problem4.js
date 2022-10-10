2c50f4fe6efdfd9a4723d34afc2d45da7f07a10d
abhijeetsenguptaa <101327881+abhijeetsenguptaa@users.noreply.github.com>
2022-10-10 21:02:02 +0530

    Logical Operators

---
+++ Logical-Operators/problem4.js
@@ -0,0 +1,12 @@
+// Given 3 numbers (all different values), print which is greatest
+
+
+let a = 90 ;
+let b = 70 ;
+let c = 30 ;
+
+a>b && a>c ?
+  console.log("A is greater"):
+b>c && b>a ?
+  console.log("B is greater"):
+  console.log("C is greater");
---
+++ Logical-Operators/problem5.js
@@ -0,0 +1,21 @@
+// Given the days of the week in short format "Sun", "Mon" ... print in long format "Sunday", "Monday", ...
+
+
+  let day = "Sun";
+
+switch(day){
+  case "Sun":console.log("Sunday");
+  break;
+  case "Mon":console.log("Monday");
+  break;
+  case "Tues":console.log("Tuesday");
+  break;
+  case "Wed":console.log("Wednesday");
+  break;
+  case "Thurs":console.log("Thursday");
+  break;
+  case "Fri":console.log("Friday");
+  break;
+  case "Sat":console.log("Saturday");
+  break;  
+}
